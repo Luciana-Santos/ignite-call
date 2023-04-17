c844d40340746afc9d5cc7bb7e49c1175f9925dc
Luciana Santos <github@luciana.dev>
2023-04-17 18:25:07 -0300

    Persistindo usuário no Prisma

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -7,6 +7,7 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { api } from '@/lib/axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -42,7 +43,14 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   const handleRegister = async (data: registerFormData) => {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
