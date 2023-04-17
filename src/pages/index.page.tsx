461b24cd5841b783c31de78609f68c784730a7c0
Luciana Santos <github@luciana.dev>
2023-04-17 15:08:55 -0300

    estrutura visual da home

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,9 +1,1 @@
-import { Heading } from '@ignite-ui/react'
-
-export default function Home() {
-  return (
-    <>
-      <Heading>Lets get it started!</Heading>
-    </>
-  )
-}
+export { default } from './home'
