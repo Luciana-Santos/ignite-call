4f71d7e17c0f500bbfa68330956dc01811723a33
Luciana Santos <github@luciana.dev>
2023-04-18 17:50:04 -0300

    Criando o usuário no adapter

---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,17 @@
+import 'next-auth'
+
+declare module 'next-auth' {
+  interface User {
+    id: string
+    name: string
+    email: string
+    username: string
+    avatar_url: string
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  interface Session {
+    user: User
+    expires_at: DateTime?
+  }
+}
